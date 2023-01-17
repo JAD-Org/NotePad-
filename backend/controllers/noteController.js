59fa631d193fe30360cae0a5b59b5ae975474762
alyssonaldrin <alysson_cdf@hotmail.com>
2023-01-16 21:39:52 -0300

    Basico rotas

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,14 +1,79 @@
+import { ObjectID } from "bson";
 import client from "../database/mongo.js";
 
-const saveNote = async (req, res) => {};
+const saveNote = async (req, res) => {
+  const note = req.body;
 
-const listNotes = async (req, res) => {
-  res.status(200).send("teste");
+  try {
+    await client.connect();
+    const notesCollection = client.db("notepad").collection("notes");
+    await notesCollection.insertOne(note);
+
+    res.status(201).send("Nota criada.");
+  } catch {
+    res.status(400).send("Falha ao salvar.");
+  } finally {
+    client.close();
+  }
 };
 
-const deleteNote = async (req, res) => {};
+const listNotes = async (req, res) => {
+  //Adicionar logica de filtro por query
+  try {
+    await client.connect();
+    const notesCollection = client.db("notepad").collection("notes");
+    const notes = await notesCollection.find().toArray();
 
-const updateNote = async (req, res) => {};
+    res.status(200).send(notes);
+  } catch {
+    res.status(400).send("Falha ao listar.");
+  } finally {
+    client.close();
+  }
+};
+
+const deleteNote = async (req, res) => {
+  //Adicionar logica caso nao exista nota para aquele id
+  const { id } = req.params;
+
+  try {
+    await client.connect();
+    const notesCollection = client.db("notepad").collection("notes");
+    await notesCollection.deleteOne({ _id: new ObjectID(id) });
+
+    res.status(200).send("Nota deletada com sucesso!");
+  } catch {
+    res.status(400).send("Falha ao deletar.");
+  } finally {
+    client.close();
+  }
+};
+
+const updateNote = async (req, res) => {
+  //Adicionar logica caso nao exista nota para aquele id
+  const { title, content } = req.body;
+  const { id } = req.params;
+
+  try {
+    await client.connect();
+    const notesCollection = client.db("notepad").collection("notes");
+    await notesCollection.updateOne(
+      { _id: new ObjectID(id) },
+      {
+        $set: {
+          title,
+          content,
+        },
+      }
+    );
+
+    res.status(200).send("Nota atualizada!");
+  } catch {
+    res.status(400).send("Falha ao atualizar.");
+  } finally {
+    client.close();
+  }
+};
 
 export default {
   saveNote,
--- backend/database/mongo.js
+++ backend/database/mongo.js
@@ -1,3 +1,5 @@
 import { MongoClient } from "mongodb";
 
-export default new MongoClient("mongodb://localhost:27017");
+export default new MongoClient(
+  "mongodb+srv://alyssonaldrin:gE0hln8KZwkOOGRf@notepad.h1xhm4j.mongodb.net/?retryWrites=true&w=majority"
+);
