7d4923e94b5b83c0d167aa16d00cd13795073c7c
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-02-10 18:06:06 -0300

    Configuração inicial de formulário de registro de usuário

--- frontend/src/pages/register/index.jsx
+++ frontend/src/pages/register/index.jsx
@@ -2,6 +2,13 @@ import "./style.css";
 
 export function Register() {
 	return (
-		<></>
+		<form action='#' method='post'>
+			<input id='username' type='text' name='username'></input>
+			<input id='password' type='password' name='password'></input>
+
+			<button type='submit'></button>
+
+			<a href='#'>Login</a>
+		</form>
 	);
-}
+};
