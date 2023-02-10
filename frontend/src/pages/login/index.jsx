5fe2f568b0fc0e262e41e616a53e2c736efe1211
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-02-10 18:06:35 -0300

    Configuração inicial de formulário de login de usuário

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -2,6 +2,13 @@ import "./style.css";
 
 export function Login() {
 	return (
-		<></>
+		<form action='#' method='post'>
+			<input id='username' type='text' name='username'></input>
+			<input id='password' type='password' name='password'></input>
+
+			<button type='submit'></button>
+
+			<a href='#'>Register</a>
+		</form>
 	);
-}
+};
