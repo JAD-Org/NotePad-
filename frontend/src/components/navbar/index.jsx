3e4370dd53e825550fcba550fb573323ce688f8b
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-02-14 21:26:55 -0300

    Feito botões de login e registro só visíveis se não já logado

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 export function NavBar() {
+    const isLoggedIn = !!localStorage.getItem("token");
     return (
         <nav className='navbar'>
             <Link to='/'>Página principal</Link>
-            <Link to='/register'>Registrar-se</Link>
-            <Link to='/login'>Login</Link>
+            {
+                !isLoggedIn ?
+                <Link to='/register'>Registrar-se</Link> :
+                <></>
+            }
+            {
+                !isLoggedIn ?
+                <Link to='/login'>Login</Link> :
+                <></>
+            }
         </nav>
     )
 }
