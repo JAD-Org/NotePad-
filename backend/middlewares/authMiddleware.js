b974dbf441833f1683a70abf2027b56750341122
Alysson Aldrin <alysson_cdf@hotmail.com>
2023-02-14 23:54:26 -0300

    Adicionando redirect para login caso n esteja logado

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 export const isAuthenticated = (req, res, next) => {
-  if (req.headers.authorization) {
+  if (req.headers.authorization && req.headers.authorization !== "null") {
     next();
   } else {
     res.status(401).json("Usuário não autenticado.");
--- frontend/src/pages/main/index.jsx
+++ frontend/src/pages/main/index.jsx
@@ -5,45 +5,48 @@ import { Note, AddNote, SearchNote, NavBar } from "./../../components";
 import api from "./../../api";
 import { useEffect, useState } from "react";
 import { Logout } from "../../components/logout";
+import { useNavigate } from "react-router-dom";
 
 export function Main() {
-	const [notas, setNotas] = useState([]);
-	const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
-	const handleSearch = (search) => {
-		setSearch(search);
-	};
+  if (localStorage.getItem("token") === null) {
+    navigate("/login");
+  }
 
-	useEffect(() => {
-		(async () => {
-			const res = await api.read(search);
-			setNotas(res === 'Usuário não autenticado.' ? [] : res);
-		})();
-	}, [search]);
+  const [notas, setNotas] = useState([]);
+  const [search, setSearch] = useState("");
 
-	return (
-		<div>
-			<NavBar/>
-			{
-				localStorage.getItem("token") ?
-				<Logout/> :
-				<></>
-			}
-			<h1>TD Notes</h1>
-			<SearchNote
-				handleSearch={(search) => {
-					handleSearch(search);
-				}}
-			/>
-			<AddNote />
-			{notas.map((nota, index) => (
-				<Note
-					key={index}
-					id={nota._id}
-					titulo={nota.title}
-					conteudo={nota.content}
-				/>
-			))}
-		</div>
-	);
+  const handleSearch = (search) => {
+    setSearch(search);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const res = await api.read(search);
+      setNotas(res === "Usuário não autenticado." ? [] : res);
+    })();
+  }, [search]);
+
+  return (
+    <div>
+      <NavBar />
+      {localStorage.getItem("token") ? <Logout /> : <></>}
+      <h1>TD Notes</h1>
+      <SearchNote
+        handleSearch={(search) => {
+          handleSearch(search);
+        }}
+      />
+      <AddNote />
+      {notas.map((nota, index) => (
+        <Note
+          key={index}
+          id={nota._id}
+          titulo={nota.title}
+          conteudo={nota.content}
+        />
+      ))}
+    </div>
+  );
 }
